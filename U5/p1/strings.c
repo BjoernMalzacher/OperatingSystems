fc39656f2d38f6237a0390abf4f29aad55f3801a
Bjoern <malzacher.bjoern@gmail.com>
2021-11-27 16:08:28 +0100

    gg ez

--- U5/p1/strings.c
+++ U5/p1/strings.c
@@ -5,10 +5,12 @@
  */
 size_t stringlength(const char *s)
 {
-    (void) s;
-    
-    // TODO: Add code here.
-    return 0;   
+    size_t count = 0 ;
+    while (*s != '\0') {
+        count +=1;
+        s +=1;
+    }   
+    return count;   
 }
 
 /**
@@ -16,13 +18,20 @@ size_t stringlength(const char *s)
  * The caller is responsible to free the returned string.
  * Returns NULL on any error.
  */
-char *stringconcat(const char *s1, const char *s2)
-{
-    (void) s1;
-    (void) s2;
-   
-    // TODO: Add code here.
-    return NULL;   
+char *stringconcat(const char *s1, const char *s2) {
+    char* concat = malloc(stringlength(s1)+stringlength(s2));
+    char* startpoint= concat;
+    while (*s1 != '\0') {
+        *concat = *s1;
+        concat+=1; 
+        s1+=1;
+    }
+    while (*s2 !='\0') {
+        *concat = *s2;
+        concat +=1;
+        s2+=1;
+    }
+    return startpoint;   
 }
 
 /**
@@ -33,6 +42,24 @@ char *stringconcat(const char *s1, const char *s2)
  */
 char **stringsplit(const char *toSplit, char delimiter)
 {
+    char** splittedArrays = malloc(stringlength(toSplit));
+    char *array;
+
+    int count = 0;
+    while (*toSplit !='\0')
+    {
+        if(*toSplit == delimiter){
+            *splittedArrays = prefix;
+            *splittedArrays +=1;
+            free(array); 
+            char *newArray;
+            array = newArray;
+        }
+        *array = *toSplit;
+        array +=1; 
+        toSplit+=1;
+    }// doesnt work;
+     
     (void) toSplit;
     (void) delimiter;
 
