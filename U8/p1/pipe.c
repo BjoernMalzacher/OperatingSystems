7b0d1670b84815c8fb6e346db3532cfb9b701a16
Bjoern <malzacher.bjoern@gmail.com>
2022-01-07 11:49:02 +0100

    u gay

--- U8/p1/pipe.c
+++ U8/p1/pipe.c
@@ -8,14 +8,15 @@
 #include <stdlib.h> /* For exit */
 #include <fcntl.h>
 #include <errno.h>
-
+#include <stdio.h>
 int run_program(char *file_path, char *argv[])
 {
 
     if ((file_path == NULL) || (argv == NULL)) {
         return -1;
     }
-
+    int* mailbox = malloc(sizeof(int)*2); 
+    pipe2(mailbox, O_CLOEXEC);
     // -------------------------
     // TODO: Open a pipe
     // -------------------------
@@ -27,7 +28,7 @@ int run_program(char *file_path, char *argv[])
 
         // Replace program
         execvp(file_path, argv);
-
+        write(*(mailbox+1),&child_pid,sizeof(int));
         // -------------------------
         // TODO: Write the error on the pipe
         // -------------------------
@@ -46,6 +47,14 @@ int run_program(char *file_path, char *argv[])
             hadError = 1;
             errno = ECANCELED;
         } else {
+            close(*(mailbox+1));
+            int buffer;
+            read(*mailbox,&buffer, sizeof(int));
+            printf("%i:::\n",buffer);
+          
+        /*    if(buffer == -1){
+                return -1;
+            }*/
             // -------------------------
             // TODO: If there was an execvp error in the child, set errno
             //       to the value execvp set it to.
--- U8/p2/message_queue.c
+++ U8/p2/message_queue.c
@@ -3,6 +3,7 @@
 #include <sys/stat.h>        /* For mode constants */
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
 /*
  * The commands supported by the server
@@ -39,36 +40,41 @@ typedef struct _Message {
 
 mqd_t startClient(void)
 {
-    // TODO: Open the message queue previously created by the server
-    return -1;
+    
+    return mq_open(QUEUE_NAME, O_WRONLY);
 }
 
 int sendExitTask(mqd_t client)
 {
-    (void)client;
+    Message m; 
+    m.command = CmdExit;
+    m.parameter1 = 0; 
+    m.parameter2 = 0; 
+    if(mq_send(client, (char*)&m,sizeof(m),0) == -1){return -1;}
+    return 0;
 
-    // TODO: Send the exit command to the server.
-    return -1;
+   
 }
 
 int sendAddTask(mqd_t client, int operand1, int operand2)
 {
-    (void)client;
-    (void)operand1;
-    (void)operand2;
+    Message m; 
+    m.command = CmdAdd;
+    m.parameter1 =operand1;
+    m.parameter2 = operand2;
+    if(mq_send(client, (char*) &m,sizeof(m),0) == -1){return -1;}
+    return 0;
 
-    // TODO: Send the add command with the operands
-    return -1;
 }
 
 int sendSubtractTask(mqd_t client, int operand1, int operand2)
 {
-    (void)client;
-    (void)operand1;
-    (void)operand2;
-
-    // TODO: Send the sub command with the operands
-    return -1;
+    Message m; 
+    m.command = CmdSubtract;
+    m.parameter1 =operand1;
+    m.parameter2 = operand2;
+    if(mq_send(client, (char*) &m,sizeof(m),0) == -1){return -1;}
+    return 0;
 }
 
 int stopClient(mqd_t client)
@@ -81,6 +87,48 @@ int stopClient(mqd_t client)
 
 int runServer(void)
 {
-    // TODO: Implement the server
-    return -1;
+    int error = 0; 
+    struct mq_attr attr;
+    attr.mq_flags =0;
+    attr.mq_curmsgs =0;
+    attr.mq_maxmsg = 10;    
+    attr.mq_msgsize = sizeof(Message);
+    mqd_t queue = mq_open(QUEUE_NAME,O_RDONLY | O_CREAT | O_EXCL,  S_IWUSR | S_IWGRP | S_IWOTH,&attr);
+    if(((int)queue) == -1) {
+        printf("%i::%i", errno,EEXIST);   
+        return -1;   
+    }   
+    int run = 1;
+    Message* val = malloc(100);
+    do{
+        
+        if(mq_receive(queue,( char*) val,sizeof(val),NULL) != sizeof(val)){error =-1;}
+        switch (val->command)
+        {
+        case CmdExit:
+            run =0; 
+            break;
+        
+        case CmdAdd:
+            printf(FORMAT_STRING_ADD, val->parameter1,val->parameter2, val->parameter1+val->parameter2);
+            break;
+        
+        case CmdSubtract:
+            printf(FORMAT_STRING_SUBTRACT, val->parameter1,val->parameter2, val->parameter1-val->parameter2);
+            break;
+        
+        default:
+            run =0;
+            error = 1;
+            break;
+        }
+
+    }while (run);
+       if (mq_close(queue) == -1)  {
+        error = 1;
+    }
+    if (mq_unlink((char*)&queue) == -1) {
+        error = 1;
+    }
+    return error? -1 : 0;
 }
