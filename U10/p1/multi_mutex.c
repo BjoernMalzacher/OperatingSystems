5d97185cefc01a1d79aa27450ffc5e025b729f2e
Bjoern <malzacher.bjoern@gmail.com>
2022-02-18 17:27:39 +0100

    lololololo

--- U10/p1/multi_mutex.c
+++ U10/p1/multi_mutex.c
@@ -2,21 +2,34 @@
 #include <unistd.h>
 #include <string.h>
 #include <alloca.h>
+#include <pthread.h>
 
 #include "multi_mutex.h"
 
 int multi_mutex_unlock(pthread_mutex_t **mutexv, int mutexc)
 {
-    (void) mutexv;
-    (void) mutexc;
-    return -1;
+    for (int i = 0; i < mutexc; i++) {
+        if(pthread_mutex_unlock(*(mutexv+i))< 0)
+            return -1;
+
+    }
+    return 0;
 }
 
 int multi_mutex_trylock(pthread_mutex_t **mutexv, int mutexc)
 {
-    (void) mutexv;
-    (void) mutexc;
-    return -1;
+     
+    for (int i = 0; i < mutexc; i++){
+        if(pthread_mutex_trylock(*(mutexv)) < 0){
+            for (int j = 0; j <= i; j++){
+                pthread_mutex_unlock(*(mutexv+j));
+        
+            }
+            return -1;       
+        }
+
+    }
+    return 0;
 }
 
 int multi_mutex_lock(pthread_mutex_t **mutexv, int mutexc)
